ce149c86777fbac388f7f1f7f0177c0f017d0cb2
unknown <vincentgirardin@ikmail.com>
2026-04-15 20:15:30 +0200

    final try

--- seed_db.py
+++ seed_db.py
@@ -49,7 +49,7 @@ def seed_database():
         # 2. Insertion des données via String URL
         db_url_string = (
             f"postgresql+psycopg2://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
-            f"@{os.getenv('DB_HOST', 'localhost')}:5432/{os.getenv('DB_NAME')}"
+            f"@{os.getenv('DB_HOST', 'localhost')}:{os.getenv('DB_PORT', '5432')}/{os.getenv('DB_NAME')}"
         )
 
         pdf.to_sql(
